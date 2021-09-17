65fd9ebfedd3dc0f9ce286694bc5b159f6e839af
Dieguerson <voices113@hotmail.com>
2021-09-16 23:40:43 -0300

    Ejercicios clase 1 + complementarios

--- wanLibJS.js
+++ wanLibJS.js
@@ -13,10 +13,11 @@ var diferenciaPadreMadre;
 var diferenciaMadrePadre;
 var edad;
 
-alert("Hola! Te voy a pedir mil datos y después voy a dejar que JS y la tecnología hagan su magia para contestar preguntas que nadie hizo. Gracias por tu cooperación. Y perdón... <3")
-
 nombre = prompt("Cuál es tu nombre?");
 apellido = prompt("Y tu apellido?");
+alert("Hola, " + nombre + " " + apellido + ", cómo estás?");
+alert("Ahora te voy a pedir mil datos y después voy a dejar que JS y la tecnología hagan su magia para contestar preguntas que nadie hizo. Gracias por tu cooperación. Y perdón... <3")
+
 primerNumero = prompt("Número Favorito sin decimales?");
 segundoNumero = prompt("Número Favorito con decimales?");
 ladoCuadrado = prompt("Inventá la medida de un lado de un cuadrado!");
@@ -24,33 +25,32 @@ edadMadre = prompt("Qué edad tiene tu madre?");
 edadPadre = prompt("Qué edad tiene tu padre?");
 anoNacimiento = prompt("En que año naciste?");
 
-alert("Hola, " + nombre + " " + apellido + ", cómo estás?");
-
 console.log("Tu nombre es: " + nombre);
 console.log("Tu apellido es: " + apellido);
 console.log("Tu número favorito sin decimales es: " + primerNumero);
-console.log("Tu número favorito con decimales es: " + segundoNumero);
+console.log("Y tu número favorito con decimales es: " + segundoNumero);
 console.log("El lado de tu cuadrado es: " + ladoCuadrado);
-console.log("La edad de tu madre es: " + edadMadre);
-console.log("Y la de tu padre es: " + edadPadre);
+console.log("La edad de tu madre es: " + edadMadre + " años");
+console.log("Y la de tu padre es: " + edadPadre + " años");
+anoActual = new Date().getFullYear();
 console.log("Este es el año " + anoActual);
 console.log("Y tu año de nacimiento es: " + anoNacimiento);
 
 operaciones = parseInt(primerNumero) + parseFloat(segundoNumero);
-console.log("La suma de tus número favoritos da: " + operaciones);
+console.log("La suma de tus números favoritos da: " + operaciones);
 operaciones = parseInt(primerNumero) * parseFloat(segundoNumero);
-console.log("La multiplicación de tus número favoritos da: " + operaciones);
-console.log("La resta de tus numeros favoritos da: " + (parseInt(primerNumero) - parseFloat(segundoNumero)));
-console.log("La división de tus numeros favoritos da: " + (parseInt(primerNumero) / parseFloat(segundoNumero)));
+console.log("La multiplicación de tus números favoritos da: " + operaciones);
+console.log("La resta de tus números favoritos da: " + (parseInt(primerNumero) - parseFloat(segundoNumero)));
+console.log("La división de tus números favoritos da: " + (parseInt(primerNumero) / parseFloat(segundoNumero)));
 areaCuadrado = ladoCuadrado * ladoCuadrado;
 console.log("El área del cuadrado que inventaste es: " + areaCuadrado);
-console.log("El consecutivo de tu número favorito sin decimales es: " + (primerNumero + 1));
-console.log("Y el consecutivo de tu número favorito con decimales es: " + segundoNumero++);
+console.log("El consecutivo de tu número favorito sin decimales es: " + (parseInt(primerNumero) + 1));
+console.log("Y el consecutivo de tu número favorito con decimales es: " + (parseFloat(segundoNumero) + 1));
 diferenciaPadreMadre = edadPadre - edadMadre;
-console.log("La diferencia entre la edad de tu padre y la de tu madre es: " + diferenciaPadreMadre);
+console.log("La diferencia entre la edad de tu padre y la de tu madre es: " + diferenciaPadreMadre + " años");
 diferenciaMadrePadre = edadMadre - edadPadre;
-console.log("Y la diferencia entre tu madre y tu padre: " + diferenciaMadrePadre);
-anoActual = new Date().getFullYear();
+console.log("Y la diferencia entre tu madre y tu padre: " + diferenciaMadrePadre + " años");
 edad = anoActual - parseInt(anoNacimiento);
-console.log("Tenés " + edad + " años");
+console.log("Y vos tenés " + edad + " años");
 
+alert("Gracias! Sos Crack!");
