b4de74c1651397051d0c726641ca9bb7fa7a7478
Dieguerson <voices113@hotmail.com>
2021-11-19 19:23:19 -0300

    Desafío 14 y 3° Preentrega

---
+++ Clase14/claseCatorce.js
@@ -0,0 +1,37 @@
+//AJAX: Asynchronous JS and XML. También trabaja con texto plano y JSON.
+//Consultas asincrónicas: No recargan la página al devolver la respuesta, solo la parte necesaria. Todo a través de protocolo HTTP.
+//HTTP: Protocolo con verbos. GET(traer información del servidor), POST (mandar información al servidor), PUT, DELETE, etc.
+//Metodo GET (en jQuery): (ver diapo de GET y POST)
+
+let urlJSON = "https://jsonplaceholder.typicode.com/posts";
+
+$("#botonGET").click(function(){$.get(urlJSON , function(datos){console.log(datos)})}); //Se puede recorrer con métodos para recorrer arrays. Revisar documentación jQuery.get()
+//POST necesita como argumentos la URL, la infrmación, la respuesta y el estado.
+
+//Geolocalización, API HTML
+
+let geo = navigator.geolocation.getCurrentPosition(ubicacion);
+
+function ubicacion(position){
+    console.log("latitud" + position.coords.latitude);
+    console.log("longitud" + position.coords.longitude);
+}
+
+//API: Application Programming Interfaces. Reglas y especificaciones que las aplicaciones siguen para comunicarse
+
+//let clima = "http://api.openweathermap.org/data/2.5/weather?q=Cordoba&appid=beaf65e31e66a3325b6aad148e509cc8"
+
+//$("#botonClima").click(function(){$.get(clima , function(datos){console.log(datos)})})
+$.ajax({
+    url:`http://api.openweathermap.org/data/2.5/weather`,
+    type: "GET",
+    data:{
+        q: 'Buenos Aires',
+        appid: 'beaf65e31e66a3325b6aad148e509cc8',
+        dataType: 'jsonp',
+        units: 'metric'
+    },
+    success: function(data){
+        console.log(data);
+    }
+});//Revisar grabación
--- ProyectoFinal/js/simDieChar.js
+++ ProyectoFinal/js/simDieChar.js
@@ -1,17 +1,51 @@
+let dndData = "https://www.dnd5eapi.co/api";
+
 $("#openModal").click(function(){$("#modal").addClass("d-block").removeClass("d-none");});
 $("#closeModal").click(function(){setTimeout(function(){$("#modal").removeClass("d-block").addClass("d-none");},500)});
 $("#openModal , #closeModal").click(function(){$("#modalContent").slideToggle("slow");});
 $("#logSend").click(access);
 $("#logIn").keypress(function(e){let key = e.which; if(key == 13){access()}});
+$("#signUp , #back").click(function(){
+    $("#userCreator").toggleClass("d-block d-none");
+    $("#logIn").toggleClass("d-flex d-none");
+});
+let user = "";
+let userData;
 function access(){
-    if ($("#userName").val() == "Admin" && $("#pass").val() == "Pr0baNd0"){
-        $("#logIn").toggleClass("d-block d-none");
-        $("#raceCreator").toggleClass("d-block d-none");
+    if ($("#pass").val() == JSON.parse(localStorage.getItem($("#userName").val())).password){
+        user = $("#userName").val();
+        userData = JSON.parse(localStorage[$("#userName").val()]);
+        charCol = userData.characters;
+        chrLstCrtr();
+        $("#logIn").html(`<fieldset>
+                                <legend class="fw-bold p-1 pt-0">Log In</legend>
+                                <p class="w-100 text-center fs-4 mt-2">You are now logged in!</p>
+                                </legend>
+                          </fieldset>`);
     }else{
         $("#logIn").append(`<p class="w-100 text-center fs-4 mt-2">That's not a valid username or password.</p>`);
         setTimeout(function(){$("#logIn").children().last().remove();},1300);
     } 
 }
+
+$("#userSend").click(function(){
+    let i = 0;
+    let j = 0;
+    while (i <= localStorage.length){
+        if ($("#newUserName").val() == localStorage.key(i)){
+            j++
+        }
+        i++
+    } 
+    if(j != 0){
+        $("#userCreator").append(`<p class="w-100 text-center fs-4 mt-2">This user name already exists!</p>`);
+        setTimeout(function(){$("#userCreator").children().last().remove();},1500);
+    }else{
+        const userData = JSON.stringify({password: $("#userPass").val() , characters: charCol});
+        localStorage.setItem ($("#newUserName").val() , userData);
+        $("#userCreator").append(`<p class="w-100 text-center fs-4 mt-2">Congratulations! Your user has been created!</p>`);
+        setTimeout(function(){$("#userCreator").children().last().remove();},1500);}
+});
 
 let faceAmm = 0;
 $(".dieSim").click(dieFace);
@@ -69,7 +103,7 @@ function hiLo(data) {
 }
 
 class character{
-    constructor (name , selectedRace , STR , DEX , CON , size , characteristics){
+    constructor (name , selectedRace , STR , DEX , CON , INT , WIS , CHA , size , characteristics){
         this.namChar = name;
         this.raceChar = selectedRace;
         this.strength = STR;
@@ -78,20 +112,35 @@ class character{
         this.dexMod = Math.floor((this.dexterity - 10) / 2);
         this.constitution = CON;
         this.conMod = Math.floor((this.constitution - 10) / 2);
+        this.intelligence = INT;
+        this.intMod = Math.floor((this.intelligence - 10) / 2);
+        this.wisdom = WIS;
+        this.wisMod = Math.floor((this.wisdom - 10) / 2);
+        this.charisma = CHA;
+        this.chaMod = Math.floor((this.charisma - 10) / 2);
         this.size = size;
         this.characteristics = characteristics;
     }
 }
-let STR = 0 , DEX = 0 , CON = 0 , charCol=[];
+let STR = 0 , DEX = 0 , CON = 0 , INT = 0 , WIS = 0 , CHA = 0 , charCol=[];
 $("#genesis").click(function(){
                         STR = 0;
                         DEX = 0;
                         CON = 0;
+                        INT = 0;
+                        WIS = 0;
+                        CHA = 0;
+                        characteristics = "";
+                        size = "";
                         stats();
                         raceSelect($("#race").val());
-                        const hero = new character($("#charName").val() , selectedRace , STR , DEX , CON , selectedRaceSize , characteristics);
+                        setTimeout(function(){const hero = new character($("#charName").val() , selectedRace , STR , DEX , CON , INT , WIS , CHA , size , characteristics);
                         charCol.push(hero);
-                        chrLstCrtr();
+                        if (user != ""){
+                            userData.characters = charCol;
+                            localStorage[user] = JSON.stringify(userData);
+                        }
+                        chrLstCrtr();},300)
                         $("body").append(`<section id="genMsj" class="position-fixed top-0 start-0 w-100 h-100 p-4 overflow-auto modal__background--color modal--depth" style="display: none;">
                                             <article id="genCont"class="position-absolute top-50 start-50 translate-middle border border-2 py-3 m-auto bg-success card--border" style="width: 0px; height: 0px;">
                                                 <h3 class="text-center fw-bold w-100 m-0 px-3" style="display: none;">Congratulations!</h3>
@@ -101,7 +150,7 @@ $("#genesis").click(function(){
                         $("#genMsj").fadeIn("fast").delay(3600).fadeOut("fast");
                         $("#genCont").animate({width:"300px"} , "slow").animate({height:"100px"} , "slow").delay(1100).animate({height:"0px"} , "slow").animate({width:"0px"} , "slow").hide(1);
                         $("#genCont *").delay(800).show("normal").delay(1100).hide("normal");
-                        setTimeout(function(){$("body").children().last().remove();},4000);
+                        setTimeout(function(){$("body").children().last().remove();},4000);;
 });
 $("#openCharModal").click(function(){$("#charModal").addClass("d-block").removeClass("d-none");});
 $("#closeCharModal").click(function(){setTimeout(function(){$("#charModal").removeClass("d-block").addClass("d-none");},500)});
@@ -116,11 +165,17 @@ function chrLstCrtr(){
                                        <li class="ms-5"> <b>Strength: </b>${char.strength}</li>
                                        <li class="ms-5"> <b>Dexterity: </b>${char.dexterity}</li>
                                        <li class="ms-5"> <b>Constitution: </b>${char.constitution}</li>
+                                       <li class="ms-5"> <b>Intelligence: </b>${char.intelligence}</li>
+                                       <li class="ms-5"> <b>Wisdom: </b>${char.wisdom}</li>
+                                       <li class="ms-5"> <b>Charisma: </b>${char.charisma}</li>
                                    </ul>
                                    <ul><b>Stats Modifiers:</b>
                                        <li class="ms-5"> <b>Strength: </b>${char.strMod}</li>
                                        <li class="ms-5"> <b>Dexterity: </b>${char.dexMod}</li>
                                        <li class="ms-5"> <b>Constitution: </b>${char.conMod}</li>
+                                       <li class="ms-5"> <b>Intelligence: </b>${char.intMod}</li>
+                                       <li class="ms-5"> <b>Wisdom: </b>${char.wisMod}</li>
+                                       <li class="ms-5"> <b>Charisma: </b>${char.chaMod}</li>
                                    </ul>
                                    <p><b>Size: </b>${char.size}</p>
                                    <p><b>Characteristics: </b>${char.characteristics}</p>
@@ -134,13 +189,19 @@ function stats(){
             STR = three();
             DEX = three();
             CON = three();
-        }while(STR === 0 || DEX === 0 || CON === 0)
+            INT = three();
+            WIS = three();
+            CHA = three();
+        }while(STR === 0 || DEX === 0 || CON === 0 || INT === 0 || WIS === 0 || CHA === 0)
     }else if ($("#statRand").val() == "4d6"){
         do{
             STR = four();
             DEX = four();
             CON = four();
-        }while(STR === 0 || DEX === 0 || CON === 0)
+            INT = four();
+            WIS = four();
+            CHA = four();
+        }while(STR === 0 || DEX === 0 || CON === 0 || INT === 0 || WIS === 0 || CHA === 0)
     }
 }
 function three(){
@@ -162,67 +223,51 @@ function four(){
     return sum;
 }
 
-$("#raceSend").click(raceCreator);
-$("#raceCreator").keypress(function(e){let key = e.which; if(key == 13){raceCreator()}});
-class newRace{
-    constructor (raceName , strMod , dexMod , conMod , raceSize, raceCharacteristics){
-        this.raceName = raceName;
-        this.strMod = strMod;
-        this.dexMod = dexMod;
-        this.conMod = conMod;
-        this.raceSize = raceSize;
-        this.raceCharacteristics = raceCharacteristics;
-    }
-}
-function raceCreator(){
-    let raceName = $("#raceName").val() , strMod = $("#strMod").val() , dexMod = $("#dexMod").val() , conMod = $("#conMod").val() , raceSize = $("#size").val() , raceCharacteristics = $("#characteristics").val();
-    const race = JSON.stringify(new newRace(raceName , strMod , dexMod , conMod , raceSize , raceCharacteristics));
-    localStorage.setItem(raceName , race);
-    $("#raceCreator").append(`<p class="w-100 text-center fs-4 mt-2">Congratulations! The ${raceName} race has been created!</p>`);
-    setTimeout(function(){$("#raceCreator").children().last().remove();},1500);
-}
 let size = "";
 let selectedRace = "";
-let selectedRaceSize = "";
 let characteristics = "";
 function raceSelect(raceChoice){
-    let storedRace = localStorage.getItem(raceChoice);
-    let ussableRace = JSON.parse(storedRace);
-    STR = STR + parseInt(ussableRace.strMod);
-    DEX = DEX + parseInt(ussableRace.dexMod);
-    CON = CON + parseInt(ussableRace.conMod);
-    characteristics = ussableRace.raceCharacteristics;
-    size = ussableRace.raceSize;
-    selectedRace = ussableRace.raceName;
-    selectedRaceSize = ussableRace.raceSize;
+    $.get(dndData+"/races/"+raceChoice , function(datos){
+        for (let i = 0 ; i < datos.ability_bonuses.length ; i++){
+            switch (datos.ability_bonuses[i].ability_score.index){
+                case "str":
+                    STR = STR + datos.ability_bonuses[i].bonus;
+                    break;
+                case "dex":
+                    DEX = DEX + datos.ability_bonuses[i].bonus;
+                    break;
+                case "con":
+                    CON = CON + datos.ability_bonuses[i].bonus;
+                    break;
+                case "int":
+                    INT = INT + datos.ability_bonuses[i].bonus;
+                    break;
+                case "wis":
+                    WIS = WIS + datos.ability_bonuses[i].bonus;
+                    break;
+                case "cha":
+                    CHA = CHA + datos.ability_bonuses[i].bonus;
+                    break;
+            }
+        }
+        characteristics = "";
+        for (let i = 0 ; i < datos.traits.length ; i++){
+            if (i <= datos.traits.length-2){
+                characteristics += datos.traits[i].name + ", ";
+            }else{
+                characteristics += datos.traits[i].name + ".";
+            }
+        }
+        size = datos.size_description;
+        selectedRace = datos.name;
+    });
 }
+
 $("#race").ready(raceList);
 function raceList(){
-    for (let i = 0; i < localStorage.length ; i++){
-        $("#race").append(`<option value="${localStorage.key(i)}">${localStorage.key(i)}</option>`);
-    }
-}
-$("#qckAdm").click(quickAdmin);
-function quickAdmin(){
-    let quickName;
-    let quickRace;
-    for (let i = 0; i < 3; i++){
-        switch (i){
-            case 0:
-                quickName = "Dwarf";
-                quickRace = JSON.stringify(new newRace("Dwarf" , 0 , 0 , 2 , "4 to 5 feet tall" , "Dwarven Resilience, Dwarven Combat Training"));
-                localStorage.setItem(quickName , quickRace);
-                break;
-            case 1:
-                quickName = "Elf";
-                quickRace = JSON.stringify(new newRace("Elf" , 0 , 2 , 0 , "5 to over 6 feet tall" , "Darkvision, Keen Senses, Fey Ancestry"));
-                localStorage.setItem(quickName , quickRace);
-                break;
-            case 2:
-                quickName = "Human";
-                quickRace = JSON.stringify(new newRace("Human" , 1 , 1 , 1 , "5 to over 6 feet tall" , "None"));
-                localStorage.setItem(quickName , quickRace);
-                break;
+    $.get(dndData+"/races" , function(datos){
+        for (let i = 0; i < datos.results.length ; i++){
+            $("#race").append(`<option value="${datos.results[i].index}">${datos.results[i].name}</option>`);
         }
-    }
+    });
 }
